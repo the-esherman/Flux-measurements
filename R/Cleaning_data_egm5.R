b746b8c460307ddc3fc3f00d7cd4a473801428a5
the-esherman <emiltheemperor@gmail.com>
2022-08-26 01:33:07 +0200

    Cleaning and flux scripts for EGM5. Adapted from the EGM4 scripts

---
+++ R/Cleaning_data_egm5.R
@@ -0,0 +1,233 @@
+# Data cleaning and combining
+# For EGM5
+# Using CPY mode (M3)
+#
+#
+# Manual:
+# The document is set up so that only a few changes should be necessary between data files.
+# To better help finding these few places, the script has the following arrows pointing to code that should be adapted as necessary:
+#
+# ═══════════════════╗
+#                    ▼
+#
+# Where this arrow is located it can be a good idea to check the settings are correct.
+#
+#
+# ### Libraries ###
+#
+library(tidyverse) # For the loading data in tibble and organizing it
+library(readxl) # For reading excel files with the "read_excel" command
+library(writexl) # For writing excel files
+#
+#
+# # Combine several data files #
+#
+# The EGM5 has a large storage, but it might still be relevant to combine several files either for same day or several days.
+# This does not assume similar length of each measurements, but later parts might!
+#
+# If an external TinyTag sensor is used for air temperature, it can be useful to not have to manually combine the egm5 file with the TinyTag data. Can also combine with an external PAR sensor logging on an EM50 logger
+#
+#
+# ════════════════════════════╗
+#                             ▼
+flux1 <- read_delim("raw_data/egm5_raw_test.txt")
+flux1 <- flux1 %>% # Here the "start," "end," and "zero" lines are cut and all the columns are processed to match either as a numeric or character value
+  filter(!is.na(CO2)) %>%
+  mutate(across(c(Plot_No, CO2, Pressure, Flow, H2O, Tsen, O2, PAR, Tsoil, Tair, Msoil, "...18", "...19", "...20", Flux, "...22"), as.numeric)) %>%
+  mutate(across(c("Tag(M3)", Rec_No, Error, Aux_V), as.character))
+flux1 <- flux1 %>%
+  separate(Time, into = c("Hour", "Min", "Sec"), sep = ":") %>% # We will not match the other measurements to the second when combining!
+  unite(Hour, Min, col = "Time", sep = ":") %>%
+  unite(Date, Time, col = "Day_ID", sep = " ")
+#
+#
+# ════════════════════════════╗ # add more files as needed
+#                             ▼
+# flux2 <- read_delim("raw_data/egm5_raw_test.txt")
+# flux2 <- flux1 %>% # Here the "start," "end," and "zero" lines are cut and all the columns are processed to match either as a numeric or character value
+#   filter(!is.na(CO2)) %>%
+#   mutate(across(c(Plot_No, CO2, Pressure, Flow, H2O, Tsen, O2, PAR, Tsoil, Tair, Msoil, "...18", "...19", "...20", Flux, "...22"), as.numeric)) %>%
+#   mutate(across(c("Tag(M3)", Rec_No, Error, Aux_V), as.character))
+# flux2 <- flux2 %>%
+#   separate(Time, into = c("Hour", "Min", "Sec"), sep = ":") %>% # We will not match the other measurements to the second when combining!
+#   unite(Hour, Min, col = "Time", sep = ":") %>%
+#   unite(Date, Time, col = "Day_ID", sep = " ")
+#
+# If combining files
+# flux_all <- bind_rows(flux1,flux2, .id = NULL) %>%
+#   rename("Plot" = Plot_No,
+#          "RecNo" = Rec_No,
+#          "ATMP" = Pressure,
+#          "Aux.V" = Aux_V,
+#          "Process" = "...18",
+#          "DC" = "...19", 
+#          "DT" = "...20", 
+#          "SRL.Rate" = Flux, 
+#          "SRQ.Rate" = "...22")
+#
+# If using only one file
+flux_all <- flux1 %>%
+  rename("Plot" = Plot_No,
+         "RecNo" = Rec_No,
+         "ATMP" = Pressure,
+         "Aux.V" = Aux_V,
+  #
+  #═══════╗
+  #       ▼
+         "PAR.inner" = PAR, # If using internal PAR: Keep original name and comment this line
+         "Process" = "...18",
+         "DC" = "...19", 
+         "DT" = "...20", 
+         "SRL.Rate" = Flux, 
+         "SRQ.Rate" = "...22")
+#
+#
+#
+# ### Combine TinyTag temperatures ###
+#
+# Load TinyTag temperature measurements
+#
+# ═════════════════════════════╗
+#                              ▼
+TinyTag1 <- read_csv("raw_data/raw_test_airT.csv", skip = 5, col_names = c("Record", "Date_time", "Max_Temp", "AirT", "Min_Temp"))
+# Change accordingly, if only using average temperature and not max and min 
+TinyTag1 <- TinyTag1 %>% # Split temperature from the unit and Date and time. Set temperature as numeric
+  separate(AirT, sep = " ", into = c("AirT", "Unit")) %>%
+  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
+  separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
+  unite(hour, min, col = "Time", sep = ":") %>%
+  unite(Date, Time, col = "Day_ID", sep = " ") %>%
+  mutate(across(c(AirT), as.numeric))
+#
+# Create data frame with only Day_ID and the average temperature
+TinyTag_mini <- tibble("Day_ID" = TinyTag1$Day_ID,"AirT" = TinyTag1$AirT)
+#
+# Combine and have 
+flux_all_Temp <- left_join(flux_all, TinyTag_mini, by = "Day_ID")
+#
+#
+# ### Combine PAR QED sensor from EM50 ###
+#
+# ══════════════════════════╗
+#                           ▼
+fPAR1 <- read_xls("raw_data/raw_test_em50_PAR.xls", col_names = c("Date_time", "PAR1", "PAR2", "PAR3", "PAR4", "PAR5"), skip = 3, col_types = c("date", "numeric", "numeric", "numeric", "numeric", "numeric"))
+fPAR <- fPAR1 %>%
+  separate(Date_time, sep = " ", into = c("Date", "Time")) %>%
+  separate(Time, sep = ":", into = c("hour", "min", "sec")) %>%
+  unite(hour, min, col = "Time", sep = ":") %>%
+  unite(Date, Time, col = "Day_ID", sep = " ")
+#
+# Remove empty columns and keep the sensor where PAR was measured on
+fDay_ID <- flux_all %>% select(Day_ID) %>% distinct(Day_ID)
+#
+fPAR_removal <- fDay_ID %>%
+  left_join(fPAR, by = "Day_ID") %>%
+  select(c("PAR1", "PAR2", "PAR3", "PAR4", "PAR5"))
+PAR_colSums <- (colSums(fPAR_removal, na.rm=T) != 0) # T if colSum is not 0, F otherwise
+Positive_PAR  <- fPAR_removal[, PAR_colSums] %>% # all the non-zero columns
+  add_column(fDay_ID, .before = TRUE)
+Positive_PAR <- Positive_PAR %>% rename("PAR" = colnames(Positive_PAR[2]))
+#
+# Combine, Temperature and PAR
+flux_all_TempPAR <- full_join(flux_all_Temp, Positive_PAR, by = "Day_ID")
+#
+fluxdata <- flux_all_TempPAR
+#
+#
+#
+# Check length of each measurement so that they are all equal
+#
+flux_recLength<-matrix(NA,nrow = max(flux_all$Plot),ncol = 2, dimnames = list(c(),c("Plot", "Recordings")))
+#matrix(NA,nrow = max(flux_all$Plot), ncol = 2)
+for (w in flux_all$Plot){
+  flux_recLength[w,2] <- length(which(flux_all$Plot==w))
+  flux_recLength[w,1] <- w
+}
+flux_recLength <- as_tibble(flux_recLength)
+#
+#
+# Move data according to whether they are 2min (121), 5min (301), 10min (601), or 15min (901)
+# For the EGM5 there is a measurement per second and then a last one. So 2min (120s) becomes 121 measurements
+#
+twoMin <- 121
+fiveMin <- 301
+tenMin <- 601
+fifteenMin <- 901
+#
+# 2min measurements
+#
+fluxdata_2min <- fluxdata %>%
+  left_join(flux_recLength, by = "Plot") %>%
+  filter(Recordings == twoMin) %>%
+  select(-c("Recordings"))
+if (nrow(fluxdata_2min)*ncol(fluxdata_2min)>0){
+  for (v in max(fluxdata_2min$Plot)){
+    fluxdata_2min$Plot[(1+twoMin*(v-1)):(twoMin+twoMin*(v-1))] = 0 + v
+  }
+  #
+  # ════════════════════════════════════╗
+  #                                     ▼
+  write_delim(fluxdata_2min,"clean_data/EGM5_Test_clean_2min.dat", delim = "\t")
+}
+#
+# 5min measurements
+#
+fluxdata_5min <- fluxdata %>%
+  left_join(flux_recLength, by = "Plot") %>%
+  filter(Recordings == fiveMin) %>%
+  select(-c("Recordings"))
+if (nrow(fluxdata_5min)*ncol(fluxdata_5min)>0){
+  for (v in max(fluxdata_5min$Plot)){
+    fluxdata_5min$Plot[(1+fiveMin*(v-1)):(fiveMin+fiveMin*(v-1))] = 0 + v
+  }
+  #
+  # ════════════════════════════════════╗
+  #                                     ▼
+  write_delim(fluxdata_5min,"clean_data/EGM5_Test_clean_5min.dat", delim = "\t")
+}
+#
+# 10min measurements
+#
+fluxdata_10min <- fluxdata %>%
+  left_join(flux_recLength, by = "Plot") %>%
+  filter(Recordings == tenMin) %>%
+  select(-c("Recordings"))
+if (nrow(fluxdata_10min)*ncol(fluxdata_10min)>0){
+  for (v in max(fluxdata_10min$Plot)){
+    fluxdata_10min$Plot[(1+tenMin*(v-1)):(tenMin+tenMin*(v-1))] = 0 + v
+  }
+  #
+  # ═════════════════════════════════════╗
+  #                                      ▼
+  write_delim(fluxdata_10min,"clean_data/EGM5_Test_clean_10min.dat", delim = "\t")
+}
+#
+# 15min measurements
+#
+fluxdata_15min <- fluxdata %>%
+  left_join(flux_recLength, by = "Plot") %>%
+  filter(Recordings == fifteenMin) %>%
+  select(-c("Recordings"))
+if (nrow(fluxdata_15min)*ncol(fluxdata_15min)>0){
+  for (v in max(fluxdata_15min$Plot)){
+    fluxdata_15min$Plot[(1+fifteenMin*(v-1)):(fifteenMin+fifteenMin*(v-1))] = 0 + v
+  }
+  #
+  # ═════════════════════════════════════╗
+  #                                      ▼
+  write_delim(fluxdata_15min,"clean_data/EGM5_Test_clean_10min.dat", delim = "\t")
+}
+#
+# leftover in need of manual checking
+#
+fluxdata_left <- fluxdata %>%
+  left_join(flux_recLength, by = "Plot") %>%
+  filter(Recordings != twoMin & Recordings != fiveMin & Recordings != tenMin & Recordings != fifteenMin)
+if (nrow(fluxdata_left)*ncol(fluxdata_left)>0){
+  #
+  # ════════════════════════════════════╗
+  #                                     ▼
+  write_delim(fluxdata_left,"clean_data/EGM5_Test_clean_left.dat", delim = "\t")
+}
+#
+# The End
---
+++ R/Flux_load_egm5.R
@@ -0,0 +1,161 @@
+# Data from the EGM5, CO2-flux-measurements
+#
+# By Emil A.S. Andersen
+# Modified from egm4 script
+#
+# Work in progress
+# Mostly works, but need datafile ".txt" without each "start," "end," and "zero" as given in the EGM5
+#
+#
+# Manual:
+# The document is set up so that only a few changes should be necessary between data files.
+# To better help finding these few places, the script has the following arrows pointing to code that should be adapted as necessary:
+#
+# ═══════════════════╗
+#                    ▼
+#
+# Where this arrow is located it can be a good idea to check the settings are correct.
+# But, before loading any files, the most important thing to keep in mind is the structure of the data file (".txt") given by the EGM5. The header specifying EGM model and software version should be removed, and the first ";" before "Plot" should also be removed.
+# This means you will end up with a header as shown below, with no lines above it.
+#
+# [Example header, this should be the very first line of the ".dat" file from the EGM4, otherwise make sure to fit the data file to this format]
+# Plot	RecNo	Day	Month	Hour	Min	CO2 Ref	mb Ref	mbR Temp	Input A	Input B	Input C	Input D	Input E	Input F	Input G	Input H	ATMP	Probe Type
+# After that a few changes can be made to fit the data differences (by noticing the arrow as shown above). 
+# First is the name of the file, followed by the end of records. For 2 min measurements there are 27 lines, but for 5 min there are 64. This should be calculated, but the lines have been given for 2, 5, 10, and 15min. Lastely is the name of the output file.
+# 
+# A few other things to keep in mind:
+# This script does not use the build-in PAR sensor, nor temperature sensor. 
+# For temperature it uses a separate TinyTag that has been logging every minute at the same time.
+# For PAR sensor it uses a PAR Photon Flux Sensor connected to an EM50 logger.
+# Both can be loaded and combined in this script to make one data file
+#
+#
+#
+# ### Libraries ###
+#
+library(tidyverse) # For the loading data in tibble and organizing it
+library(readxl) # For reading excel files with the "read_excel" command
+library(writexl) # For writing excel files
+#
+#
+#
+# ### Loading data ###
+#
+# From .dat
+#
+# ══════════════════════════════╗
+#                               ▼
+fluxdat<-read.delim("clean_data/EGM5_Test_clean_2min.dat") # Read as a tab delimited files
+# ### Main Script: calculations ###
+#
+co2<-fluxdat$CO2 #When importing from excel use 'CO2 ref', when importing from .dat it converts to CO2.Ref
+p<-fluxdat$ATMP/10 # Air pressure as measured by the EGM. Converted from mb to kPa
+#Temp<-fluxdata$Tair # Temperature measured by CPY-5 chamber
+Temp<-fluxdat$AirT # Temperature added from a TinyTag
+PAR<-fluxdat$PAR # PAR added from PAR sensor/EM50 logger
+#
+# ═╗
+#  ▼
+A<-0.0079 # m2
+#
+# ═══╗
+#    ▼
+Vol<-0.000550 # m3
+plot<-fluxdat$Plot
+time_plot<-fluxdat$RecNo # Recording number, not currently used
+time<-fluxdat$DT # DT (change in seconds) # Not currently used
+recStart<-33 # Start of records, 33s cut
+#
+# ══════╗
+#       ▼
+recEnd<-121 # End of records(121s for 2min measurements, 301s for 5min, 601s for 10min, 901s for 15min, 181s for 3min)
+#
+#
+# Change time at record start to T = 0
+Time<-vector("double")
+Time<-seq((-(recStart-1)),(recEnd-recStart)) # Make first 33s negative and end depending on last recording number
+Dt<-rep(Time,(length(plot))/recEnd)
+#
+#
+#
+# quality test #### Make a bunch of graphs in a PDF with the CO2 increase over time measured
+#
+# ════════════════╗
+#                 ▼
+pdf(paste("output/EGM5_Test_2min","_QC-2",".pdf", sep = ""))
+par(mar = c(4, 5, 2, 2), mfrow = c(2,2)) # mar-form: bottom, left, top, right # , mfrow = c(2,1)
+for (q in 1:max(plot)) {
+  #
+  ind.1 <- plot[recStart+recEnd*(q-1)] == fluxdat$Plot # Creates a TRUE/FALSE binary for the whole dataset rows. Where TRUE is only the plots corresponding to q
+  ind.dat <- which(fluxdat$Plot == plot[recStart+recEnd*(q-1)]) # locations in dataset. Where ind.1 is TRUE it finds the rows. Thus extracting the row numbers for plot q
+  #
+  c.co2.temp  <- fluxdat$CO2[ind.1] # the CO2 values that are associated with q
+  #TS.dat.temp <- seq(1, length(c.co2.temp), 1) # the time length, in recNo, from one recording to next
+  #
+  plot(c.co2.temp[recStart:recEnd]~Dt[recStart:recEnd], ylab = "ppm CO2", xlab = "Time (s)") # plot the graph. Using CO2 values for y and time (Dt) for x, but removing the first number of measurements (recStart)
+  abline(lm(c.co2.temp[recStart:recEnd]~Dt[recStart:recEnd])) # Create a linear line with lm
+  #
+  mtext(text = paste("Plot nr", plot[recStart+recEnd*(q-1)], sep = " "), side = 3, line = -2) # Write plot number in plot
+  mtext(text = round(summary(lm(c.co2.temp[recStart:recEnd]~Dt[recStart:recEnd]))$r.squared, digits = 4), side = 1, line = -2) # calculate and write the R^2 value of the linear model 
+}
+dev.off()
+#
+#
+#
+# The process of getting all data as in matlab
+# Define all vectors and matrices used
+meanT<-vector("double")
+meanP<-vector("double")
+co2trim<-vector("list")
+timetrim<-vector("list")
+fluxmod<-vector("list")
+sse<-vector("double")
+C1_fit<-matrix(NA,nrow = max(plot),ncol = 2)
+C1_fit_intcep<-vector("double") # Unused?
+f1_lin_umol<-matrix(NA,nrow = max(plot),ncol = 1)
+#
+for (i in plot){
+  meanT[i]<-mean(Temp[(recStart+recEnd*(i-1)):(recEnd+recEnd*(i-1))]) # averaging temperature over the recordings
+  meanP[i]<-mean(p[(recStart+recEnd*(i-1)):(recEnd+recEnd*(i-1))]) # averaging pressure over the recordings
+  co2trim[[i]]<-co2[(recStart+recEnd*(i-1)):(recEnd+recEnd*(i-1))] # CO2 for i=1:max(plot)
+  timetrim[[i]]<-Dt[(recStart+recEnd*(i-1)):(recEnd+recEnd*(i-1))] # time for i=1:max(plot)
+  fluxmod[[i]]<-summary(lm(co2trim[[i]]~timetrim[[i]])) # model summary
+  sse[[i]]<-deviance(lm(co2trim[[i]]~timetrim[[i]])) # sse
+  C1_fit[i,]<-cbind(fluxmod[[i]]$coefficients[1,1],fluxmod[[i]]$coefficients[2,1]) # Array of intercept and slope (b and a in the regression y=a*x+b)
+}
+#
+# linear production of CO2
+for (i in plot) {
+  f1_lin_umol[i,]<-((C1_fit[i,2]*273.15*meanP[i]/(22.4*(meanT[i]+273.15)*101.325))*(Vol/A)*1000) # µmol CO2 m-2 s-1
+  # The slope, C1_fit[i,2] is the change in CO2 concentration over time (µmol mol^-1 s^-1)
+  # meanP[i] is the average pressure of the measurement.
+  # The ideal gas at 273.15K and 101.325 kPa gives a molar volume of 22.4 dm^3 mol^-1
+  # Volume is given at m^3 and area (A) at m^2 times 1000 dm^3 m^-3
+  # meanT[i] is the average temperature in °C, converted to kelvin by adding 273.15
+  #
+  # This gives the equation with units:
+  # (µmol mol^-1 s^-1 * 273.15K * kPa / (22.4 dm^3 mol^-1 * K * 101.325 kPa)) * (m^3 / m^2) * 1000 dm^3 m^-3
+}
+#
+# Attach all data to an output file
+output<-matrix(NA,nrow = max(plot),ncol = 9) # missing P-value and F statistic
+output[,1]<-(1:max(plot)) # Plots
+for (i in plot) {
+  output[i,2]<-sse[[i]] # sse
+  output[i,3]<-fluxmod[[i]]$r.squared # R2
+  output[i,4]<-fluxmod[[i]]$df[2] # Degrees of freedom, df
+  output[i,5]<-fluxmod[[i]]$adj.r.squared # Adj. R2
+  output[i,6]<-fluxmod[[i]]$sigma #RSME
+}
+output[,7]<-C1_fit[,2] # p1
+output[,8]<-C1_fit[,1] # p2
+output[,9]<-f1_lin_umol # Linear production of CO2; µmol CO2 m-2 s-1
+colnames(output)<- c("Plot","sse","R2","df","R2_adj","rsme","p1","p2","f1_lin_umol") # For rewriting the column names
+# Write excel file
+output_xl<-as.data.frame(output)
+#
+# ═══════════════════════════════════╗
+#                                    ▼
+write_xlsx(output_xl, path = "output/EGM5_Output_Test_2min.xlsx", col_names = TRUE, format_headers = FALSE)
+#
+# The End
